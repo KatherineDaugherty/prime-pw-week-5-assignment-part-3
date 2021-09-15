4df3a705a1afc7130c187b3705ce80b3a2263b04
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-09-15 12:07:11 -0500

    made an object and variable

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,13 @@
 console.log('***** Music Collection *****')
+
+let collection = []
+
+let CDs = {
+  title: 'Light Chasers',
+  artist: 'Cloud Cult',
+  yearPublished: '2010'
+}
+
+function addToCollection( title, artist, yearPublished){
+  return Collection
+}
